538d7e669a2786f1f57dd59ee3a2c8f739a3f7c4
Jeremy Shipman <jeremy@burnbright.net>
2020-03-02 17:05:32 +1300

    Moved common event binding logic into applyShapeEventListeners util

--- examples/CharacterText/child_events.ts
+++ examples/CharacterText/child_events.ts
@@ -1,17 +1,43 @@
 import createHiDPICanvas from "../../lib/hidpi-canvas";
+
+// TODO: import this list from src dir instead
+const EventNames = [
+  "click",
+  "dblclick",
+  "mousedown",
+  "mouseout",
+  "mouseover",
+  "pressmove",
+  "pressup",
+  "rollout",
+  "rollover",
+  "added",
+  "removed",
+  "tick"
+];
+
 export default function init() {
   let canvas = createHiDPICanvas(300, 200, 2);
   document.body.appendChild(canvas);
+
+  var output = document.createElement("p");
+  document.body.appendChild(output);
+
   let stage = new createjs.Stage(canvas);
+
+  // attach all event types for demoing
+  let events = EventNames.reduce((prev, cur) => {
+    prev[cur] = () => {
+      console.log(cur);
+      output.innerHTML = cur;
+    };
+    return prev;
+  }, {});
 
   let text = new txt.CharacterText({
     text: "The fox jumped over the log.",
     font: "arimo",
-    character: {
-      click: function(event) {
-        console.log("click");
-      }
-    },
+    character: events,
     tracking: -4,
     lineHeight: 120,
     width: 600,
